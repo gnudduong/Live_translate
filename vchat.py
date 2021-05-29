afd2917593dec7995aaab4435952c9b586219f6c
gnudduong <80591335+gnudduong@users.noreply.github.com>
2021-05-29 13:24:20 +0700

    Add files via upload

--- vchat.py
+++ vchat.py
@@ -10,16 +10,18 @@ Created on Tue Mar 30 19:50:00 2021
 import speech_recognition as sr
 from googletrans import Translator
 from tkinter import *
+
 # from PIL import ImageTk,Image
 
 
 p = Translator() 
 r =sr.Recognizer()
 
+
+"""UI"""
 root = Tk()
 root.title('Voice chat')
-# root.iconbitmap('C:/Users/slims/Downloads/image/A.png')
-root.geometry('300x300')
+root.geometry('400x200')
 
 lang={
   "Afrikaans": [
@@ -170,8 +172,14 @@ lang={
     ["South Africa", "zu-ZA"]
   ]
 }
-
-i_source={'Microphone':1,'PC':3}
+mic_list=sr.Microphone.list_microphone_names()
+i_source={}
+for i,input in enumerate(mic_list):
+    if input.startswith('Microsoft')==False:
+        i_source[input]=i
+    if input.startswith('Stereo'):
+        break
+#i_source={'Microphone':1,'PC':3}
 
 LANGUAGES = {'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-cn', 'chinese (traditional)': 'zh-tw', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'he', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'korean': 'ko', 'kurdish (kurmanji)': 'ku', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lithuanian': 'lt', 'luxembourgish': 'lb', 'macedonian': 'mk', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'mongolian': 'mn', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'odia': 'or', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'scots gaelic': 'gd', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'}
 
@@ -200,11 +208,26 @@ lang2_label= Label(text='Source:')
 lang2_label.grid(row=0,column=2)
 
 
+"""cc"""
+def display(text):
+    top=Toplevel()
+    label =Label(top,text=text, font=('Times','30'), fg='black',bg='white',borderwidth=0)
+    label.master.overrideredirect(True)
+    ws = label.winfo_screenwidth() # width of the screen
+    hs = label.winfo_screenheight() # height of the screen
+    x=ws/2
+    y=int(hs/10*9)
+    label.master.geometry(('+%d+%d' % (x, y)))
+    label.master.lift()
+    label.master.wm_attributes("-topmost", True)
+    label.master.wm_attributes("-disabled", True)
+    label.master.wm_attributes("-transparentcolor", "white")
+    label.pack()
+    label.after(1500, lambda: label.master.destroy()) # Destroy the widget after 30 seconds
+    label.mainloop()
 
-
-
-
-
+    
+"""Start func"""    
 def get_info():
     input_lang=lang[lang_in.get()][0][1]
     output_lang=LANGUAGES[lang_out.get()]
@@ -212,39 +235,46 @@ def get_info():
     print(input_lang,output_lang,input_source)
 
     trans(input_lang,output_lang,input_source)
+def close():
+    global gate
+    gate= False
+    root.destroy()
 
 
 
 
 
 
-
-
-# print(sr.Microphone.list_microphone_names())
+"""Recognition and translate"""
 def trans(input_lang,output_lang,input_source):
-
+    global gate
     gate=True
 
     while gate:
 
         
         with sr.Microphone(device_index=input_source) as source:
-            print('Listenning...')
-            audio = r.listen(source,phrase_time_limit=3)
+            print('Listening...')
+            audio = r.listen(source,phrase_time_limit=5)
             try:
                 text=r.recognize_google(audio,language=input_lang)
                 # print(text)
-             
-                k = p.translate(text, dest=output_lang)
-                # trans_label= Label(text=k.text)
-                # trans_label.grid(row=3,column=0,columnspan=2)
-                print(k.text)
+                if text == 'stop listening':
+                    gate= False
+                else:    
+                    k = p.translate(text, dest=output_lang)
+                    # trans_label= Label(text=k.text)
+                    # trans_label.grid(row=3,column=0,columnspan=2)
+                    # print(k.text)
+                    display(k.text)
             except:
-                print('Bad voice')
+                print('...')
 
 start_butt=Button(root, text='Start',command=get_info)
-start_butt.grid(row=2,column=0,columnspan=3)
-            
+start_butt.grid(row=2,column=0,columnspan=2)
+
+close_butt=Button(root, text='Close',command=close)
+close_butt.grid(row=2,column=1,columnspan=1)
 
 
 root.mainloop()
