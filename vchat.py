8125b342a4eac5d91538a0960fa9ec0283897e70
gnudduong <80591335+gnudduong@users.noreply.github.com>
2021-05-29 17:51:45 +0700

    Add files via upload

--- vchat.py
+++ vchat.py
@@ -8,13 +8,12 @@ Created on Tue Mar 30 19:50:00 2021
 # voicechat
 
 import speech_recognition as sr
-from googletrans import Translator
+from google_trans_new import google_translator  
 from tkinter import *
+import threading
 
-# from PIL import ImageTk,Image
 
-
-p = Translator() 
+p = google_translator()   
 r =sr.Recognizer()
 
 
@@ -22,6 +21,7 @@ r =sr.Recognizer()
 root = Tk()
 root.title('Voice chat')
 root.geometry('400x200')
+
 
 lang={
   "Afrikaans": [
@@ -179,8 +179,7 @@ for i,input in enumerate(mic_list):
         i_source[input]=i
     if input.startswith('Stereo'):
         break
-#i_source={'Microphone':1,'PC':3}
-
+# print(i_source)
 LANGUAGES = {'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-cn', 'chinese (traditional)': 'zh-tw', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'he', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'korean': 'ko', 'kurdish (kurmanji)': 'ku', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lithuanian': 'lt', 'luxembourgish': 'lb', 'macedonian': 'mk', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'mongolian': 'mn', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'odia': 'or', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'scots gaelic': 'gd', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'}
 
 lang_in=StringVar()
@@ -200,7 +199,7 @@ lang2_label= Label(text='Output language:')
 lang2_label.grid(row=0,column=1)
 
 mic_in=StringVar()
-mic_in.set('Microphone')
+mic_in.set('Microphone Array (Realtek High ')
 drop_2 = OptionMenu(root, mic_in, *i_source)
 drop_2.grid(row=1, column=2)
 
@@ -212,19 +211,18 @@ lang2_label.grid(row=0,column=2)
 def display(text):
     top=Toplevel()
     label =Label(top,text=text, font=('Times','30'), fg='black',bg='white',borderwidth=0)
-    label.master.overrideredirect(True)
+    top.overrideredirect(True)
     ws = label.winfo_screenwidth() # width of the screen
     hs = label.winfo_screenheight() # height of the screen
-    x=ws/2
+    x=int(ws/2)
     y=int(hs/10*9)
-    label.master.geometry(('+%d+%d' % (x, y)))
-    label.master.lift()
-    label.master.wm_attributes("-topmost", True)
-    label.master.wm_attributes("-disabled", True)
-    label.master.wm_attributes("-transparentcolor", "white")
+    top.wm_geometry(('+%d+%d' % (x, y)))
+    top.lift()
+    top.wm_attributes("-topmost", True)
+    top.wm_attributes("-disabled", True)
+    top.wm_attributes("-transparentcolor", "white")
     label.pack()
-    label.after(1500, lambda: label.master.destroy()) # Destroy the widget after 30 seconds
-    label.mainloop()
+    label.after(1500, top.destroy) # Destroy the widget after 1,5 seconds
 
     
 """Start func"""    
@@ -235,6 +233,14 @@ def get_info():
     print(input_lang,output_lang,input_source)
 
     trans(input_lang,output_lang,input_source)
+def start():
+    threading.Thread(target=(get_info)).start()
+    
+def stop():
+    global gate
+    gate= False
+    print('Stopped')
+    
 def close():
     global gate
     gate= False
@@ -262,19 +268,20 @@ def trans(input_lang,output_lang,input_source):
                 if text == 'stop listening':
                     gate= False
                 else:    
-                    k = p.translate(text, dest=output_lang)
-                    # trans_label= Label(text=k.text)
-                    # trans_label.grid(row=3,column=0,columnspan=2)
-                    # print(k.text)
-                    display(k.text)
+                    translate_text = p.translate(text, lang_tgt=output_lang)  
+                    # print(translate_text)
+                    display(translate_text)
             except:
                 print('...')
 
-start_butt=Button(root, text='Start',command=get_info)
-start_butt.grid(row=2,column=0,columnspan=2)
+start_butt=Button(root, text='Start',command=start)
+start_butt.grid(row=2,column=0)
+
+stop_butt=Button(root, text='Stop',command=stop)
+stop_butt.grid(row=2,column=1)
 
 close_butt=Button(root, text='Close',command=close)
-close_butt.grid(row=2,column=1,columnspan=1)
+close_butt.grid(row=2,column=2)
 
 
 root.mainloop()
